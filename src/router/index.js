33fdc11c2416f5f99a857271b9256f1184c3e501
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2023-01-29 13:43:56 -0300

    Adicionados: loop para listar os itens, componentes de modal, item, e botão de toggle do modal

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import DashboardComponent from '../components/DashboardComponent.vue'
+import DashboardComponent from '../components/pages/DashboardComponent.vue'
 
 const routes = [
   {
@@ -7,14 +7,6 @@ const routes = [
     name: 'home',
     component: DashboardComponent
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
