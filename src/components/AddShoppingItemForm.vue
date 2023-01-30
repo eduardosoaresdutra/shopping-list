029dae690f38cf649cfd8b5b1f79f8b62376bf85
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2023-01-29 21:22:26 -0300

    Refatorações na estrutura do projeto e alterações nos componentes de modal

---
+++ src/components/AddShoppingItemForm.vue
@@ -0,0 +1,40 @@
+<script>
+    export default {
+        data() {
+            return {
+                itemName: "",
+                itemQuantity: 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .input__wrapper {
+        display: flex;
+        flex-direction: column;
+
+        div {
+            width: 100%;
+        }
+    }
+</style>
+
+<template>
+    <div class="input__wrapper d-flex justify-center align-items-center">
+        <div class="input__wrapper__form">
+            <div class="form-floating w-100">
+                <input class="form-control" type="text" id="itemName" placeholder="Leite">
+                <label for="itemName">Nome do Item</label>
+            </div>
+            <div class="form-floating">
+                <input type="number" class="form-control" id="itemQuantity" placeholder="1">
+                <label for="itemQuantity">Quantidade</label>
+            </div>
+        </div>
+        <div class="input__wrapper__buttons">
+            <button type="button" class="btn btn-primary w-75" data-bs-dismiss="modal">Adicionar Item</button>
+            <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">Fechar</button>
+        </div>
+    </div>
+</template>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,54 @@
+<script>
+    import AddShoppingItemForm from "../components/AddShoppingItemForm.vue";
+    import NewItemButton from "../components/NewItemButton.vue";
+    import ShoppingItem from "../components/ShoppingItem.vue";
+
+    export default { 
+        components: {
+            ShoppingItem,
+            AddShoppingItemForm,
+            NewItemButton
+        },
+        data() {
+            return {
+                items: [
+                    {
+                        itemName: "Leite",
+                        itemQuantity: 2
+                    },
+                ],
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .dashboard-component__wrapper {
+        width: 100vw;
+
+        &__add-item__wrapper {
+            position: absolute;
+            margin: 35vh 0;
+        }
+    }
+</style>
+
+<template>
+    <div class="dashboard-component__wrapper d-flex flex-column justify-content-center align-items-center">
+        <div class="dashboard-component__wrapper__shopping-items">
+            <div v-for="(item, index) in items" :key="index" class="item">
+                <ShoppingItem :item-name="item.itemName" :item-quantity="item.itemQuantity"></ShoppingItem>
+            </div>
+        </div>
+        <div class="dashboard-component__wrapper__add-item__wrapper modal" id="addItemModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <AddShoppingItemForm></AddShoppingItemForm>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <NewItemButton></NewItemButton>
+    </div>
+</template>
